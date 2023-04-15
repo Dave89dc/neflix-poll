c396ad4be724208beb864dd255f3077b5a520f01
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-04-15 19:10:14 +0200

    netflix-poll

--- app.js
+++ app.js
@@ -55,20 +55,79 @@ function createListForSeries() {
     const seriesArray = netflixSeriesList.series;
     for (let i = 0; i < seriesArray.length; i++) {
         const serie = seriesArray[i];
-        seriesBox.innerHtml += `<div class='serie'>
-                                    <img class='serie-img' src='${serie.imageUrl}' alt='Serie img'>
-                                    <ul class='description-list'>
-                                        <li>Title: ${serie.title}</li>
-                                        <li>Creator: ${serie.creator}</li>
-                                        <li>Seasons: ${serie.seasons}</li>
-                                        <li>Status: ${serie.ifIsCompleted}</li>
-                                        <li>Positive Votes: ${serie.upVotes}</li>
-                                        <li>Negative Votes: ${serie.downVotes}</li>
-                                    </ul>
-                                    <span class='rating-span'>Rating: ${serie.rating()}</span>
-                                    <button id='upVotes-btn${i}'><img class='votes-btn' src='./assets/thumbs-up-solid.svg' alt='up-icon'></button>
-                                    <button id='downVotes-btn${i}'><img class='votes-btn' src='./assets/thumbs-down-solid.svg' alt='down-icon'></button>
-                                </div>`;
+        // seriesBox.innerHtml += `<div id='series'>
+        //                             <img class='serie-img' src='${serie.imageUrl}' alt='Series img'>
+        //                             <ul class='description-list'>
+        //                                 <li>Title: ${serie.title}</li>
+        //                                 <li>Creator: ${serie.creator}</li>
+        //                                 <li>Seasons: ${serie.seasons}</li>
+        //                                 <li>Status: ${serie.ifIsCompleted}</li>
+        //                                 <li>Positive Votes: ${serie.upVotes}</li>
+        //                                 <li>Negative Votes: ${serie.downVotes}</li>
+        //                             </ul>
+        //                             <span class='rating-span'>Rating: ${serie.rating()}</span>
+        //                             <button id='upVotes-btn${i}'><img class='votes-btn' src='./assets/thumbs-up-solid.svg' alt='up-icon'></button>
+        //                             <button id='downVotes-btn${i}'><img class='votes-btn' src='./assets/thumbs-down-solid.svg' alt='down-icon'></button>ù
+        //                         </div>`;
+        const divSeries = document.createElement('div');
+        divSeries.classList.add('series');
+
+        const serieImg = document.createElement('img');
+        serieImg.classList.add('series-img');
+        serieImg.src = serie.imageUrl;
+
+        const seriesUl = document.createElement('ul');
+
+        const titleList = document.createElement('li');
+        const creatorList = document.createElement('li');
+        const seasonsList = document.createElement('li');
+        const statusList = document.createElement('li');
+        const positiveList = document.createElement('li');
+        const negativeList = document.createElement('li');
+
+        const titleText = document.createTextNode(`Title: ${serie.title}`);
+        const creatorText = document.createTextNode(`Creator: ${serie.creator}`);
+        const seasonsText = document.createTextNode(`Seasons: ${serie.seasons}`);
+        const statusText = document.createTextNode(`Status: ${serie.ifIsCompleted}`);
+        const positiveText = document.createTextNode(`Positive Votes: ${serie.upVotes}`);
+        const negativeText = document.createTextNode(`Negative Votes: ${serie.downVotes}`);
+
+        titleList.appendChild(titleText);
+        creatorList.appendChild(creatorText);
+        seasonsList.appendChild(seasonsText);
+        statusList.appendChild(statusText);
+        positiveList.appendChild(positiveText);
+        negativeList.appendChild(negativeText);
+
+        seriesUl.appendChild(titleList);
+        seriesUl.appendChild(creatorList);
+        seriesUl.appendChild(seasonsList);
+        seriesUl.appendChild(statusList);
+        seriesUl.appendChild(positiveList);
+        seriesUl.appendChild(negativeList);
+
+        const ratingSpan = document.createElement('span');
+        const ratingText = document.createTextNode(serie.rating());
+        ratingSpan.appendChild(ratingText);
+
+        const upVotesBtn = document.createElement('button');
+        const downVotesBtn = document.createElement('button');
+        upVotesBtn.classList.add('upVotes-btn');
+        downVotesBtn.classList.add('downVotes-btn');
+
+        const upVotesImg = document.createElement('img');
+        const downVotesImg = document.createElement('img');
+
+        upVotesBtn.appendChild(upVotesImg);
+        downVotesBtn.appendChild(downVotesImg);
+
+        divSeries.appendChild(serieImg);
+        divSeries.appendChild(seriesUl);
+        divSeries.appendChild(ratingSpan);
+        divSeries.appendChild(upVotesBtn);
+        divSeries.appendChild(downVotesBtn);
+
+        seriesBox.appendChild(divSeries);
     };
 };
 
